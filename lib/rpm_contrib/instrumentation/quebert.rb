572f3f627be9df3d2713c6341ac95f32deabb455
Steel Fu <steelfu@gmail.com>
2011-09-21 17:28:24 -0700

    Include controller instrumentation in each class

--- lib/rpm_contrib/instrumentation/quebert.rb
+++ lib/rpm_contrib/instrumentation/quebert.rb
@@ -4,19 +4,18 @@ DependencyDetection.defer do
   end
 
   executes do   
-    ::Quebert::Job.class_eval do
-      include NewRelic::Agent::Instrumentation::ControllerInstrumentation
-    end
-    
     ::Quebert::AsyncSender::ActiveRecord::RecordJob.class_eval do
+      include NewRelic::Agent::Instrumentation::ControllerInstrumentation
       add_transaction_tracer :perform, :category => :task, :params => '{:record => args[0], :meth => args[1], :args => args.slice(2, args.length - 2)}'
     end
 
     ::Quebert::AsyncSender::Instance::InstanceJob.class_eval do
+      include NewRelic::Agent::Instrumentation::ControllerInstrumentation
       add_transaction_tracer :perform, :category => :task, :params => '{:klass => args[0], :init_args => args[1], :meth => args[2], :args => args.slice(3, args.length - 3)}'
     end
 
     ::Quebert::AsyncSender::Object::ObjectJob.class_eval do
+      include NewRelic::Agent::Instrumentation::ControllerInstrumentation
       add_transaction_tracer :perform, :category => :task, :params => '{:const => args[0], :meth => args[1], :args => args.slice(2, args.length - 2)}'
     end
   end
